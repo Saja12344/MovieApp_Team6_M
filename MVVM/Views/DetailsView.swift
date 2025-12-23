03c2c5782e0683c901fb8bd41ac84154376bec01
Hadeel <ha.ada1@hotmail.com>
2025-12-23 10:39:05 +0300

    MVVM

---
+++ MVVM/Views/DetailsView.swift
@@ -0,0 +1,18 @@
+//
+//  DetailsView.swift
+//  
+//
+//  Created by Hadeel Alansari on 23/12/2025.
+//
+
+import SwiftUI
+
+struct DetailsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DetailsView()
+}
